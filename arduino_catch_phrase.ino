6f38f1c85ce6c131bfe318761807cb60146bb855
Austin Ellsworth <ellswoau@mail.gvsu.edu>
2021-02-09 23:45:23 -0500

    Just added some comments so I can remember my current thought process and keep track of how things are going to work in this.

--- arduino_catch_phrase.ino
+++ arduino_catch_phrase.ino
@@ -7,9 +7,13 @@ int teamOneScore, teamTwoScore, wordIndex, timerSeconds = 0;
 String selectedCategory = "";
 
 //constant setting to control timer countdown
-TIMERSETSECONDS = 30;
+int TIMERSETSECONDS = 20;
 
+//boolean variable to start the timer back up at N seconds
 bool timerStart = true;
+
+//variable that starts the game loop when set to true
+bool playing = false;
 
 //initialize arraylist categories of words
 //probably need to use a different arraylist type
@@ -47,17 +51,19 @@ void setup() {
     selectWord();
 }
 
+//the beginning of a timer that will have to run with the gameplay in serial
 void timer() {
-    seconds = TIMERSETSECONDS;
+    timerSeconds = TIMERSETSECONDS;
     while (timerStart) {
         delay(1000);
-        seconds--;
-        if (seconds == 0) {
-            timerStart = false
+        timerSeconds--;
+        if (timerSeconds == 0) {
+            timerStart = false;
         }
     }
 }
 
+//this draws all the text on the display
 void drawScreen() {
     //need to make it only clear display if any values changed
     if (previousWord != currentWord) {
@@ -66,14 +72,15 @@ void drawScreen() {
         showScores();
         showCurrentWord();
 
-        //this resets the previous variable values to match the current values
-        //so that the drawScreen method wont continue to clear and redraw
-        //will probably need to add scores, category, etc
+
         ResetPreviousValues();
     }
 
 }
 
+//this resets the previous variable values to match the current values
+//so that the drawScreen method wont continue to clear and redraw
+//will probably need to add scores, category, etc
 void ResetPreviousValues() {
     previousWord = currentWord;
 }
@@ -84,6 +91,8 @@ void waitForEnter() {
     }
 }
 
+//a random variable will need to eventually be used to choose words randomly
+//from the arraylist of the chosen category.
 void nextWord() {
     wordIndex = random(sizeof(testCat));
     currentWord = String (testCat[wordIndex]);
@@ -95,6 +104,7 @@ void nextWord() {
 
 }
 
+//method that prints the team scores on the bottom of the screen
 void showScores() {
     lcd.setCursor(0,3);
     lcd.print("Team1: " + String(teamOneScore));
@@ -102,6 +112,7 @@ void showScores() {
     lcd.print("Team2: " + String(teamTwoScore));
 }
 
+//method that displays the current word
 void showCurrentWord() {
     lcd.setCursor(0, 2);
     lcd.print("              ");
@@ -109,11 +120,16 @@ void showCurrentWord() {
     lcd.print(String (testCat[wordIndex]));
 }
 
+//method that shows the current select category (which is also an array list)
 void showCategory() {
     lcd.setCursor(0,1);
     lcd.print("Category: Countries");
 }
 
+
+//temporarily being used for simplicity reasons instead of the
+//nextwork method because I haven't figured out arraylists and random
+//in c++ yet
 void selectWord() {
     bool selected = false;
     while (!selected) {
@@ -139,6 +155,7 @@ void chooseCategory() {
 
 
 
+//going to have to use the loop for actual game play so that the timer will work at the same time
 void loop()
 {
     //teamTwoScore = 3;
